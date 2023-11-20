eb9e88cbd78ac119f0a17959a7306308eb98cb13
gns12 <garrettspears12@gmail.com>
2023-11-19 19:42:43 -0500

    Created LinePageHeader component and implemented page header for About section.

--- src/About/About.js
+++ src/About/About.js
@@ -1,7 +1,9 @@
+import LinePageHeader from "../PageHeaders/LinePageHeader";
+
 function About() {
     return (
-        <section id="about" className="w-screen h-[2000px] bg-orange-500">
-          <h1 className="text-white">At About Page</h1>
+        <section id="about">
+          <LinePageHeader reverseDirection={false} headerText="About Me" />
         </section>
       );
 }
---
+++ src/PageHeaders/LinePageHeader.js
@@ -0,0 +1,10 @@
+function LinePageHeader(props) {
+    return (
+        <div className={`w-5/6 flex ${props.reverseDirection ? 'flex-row-reverse' : 'flex-row'} items-center mx-auto mt-10 gap-x-10`}>
+            <h1 className="text-white text-5xl font-roboto-slab font-semibold">{props.headerText}</h1>
+            <hr className="flex-grow border-2" />
+        </div>
+    );
+}
+
+export default LinePageHeader;
