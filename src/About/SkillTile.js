f9703d3151d2b89a476371702ae982d169da9a43
gns12 <garrettspears12@gmail.com>
2024-02-03 23:35:12 -0500

    Added rest of skill tiles and fixed multi-line skill tile spacing.

--- src/About/SkillTile.js
+++ src/About/SkillTile.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 
 function SkillTile(props) {
     const [icon, setIcon] = useState(null);
+    const [isMultiLine, setIsMultiLine] = useState(false);
 
-    // Import icon dynamically based on icon image name passed into props
     useEffect(() => {
+        // Import icon dynamically based on icon image name passed into props
         const loadIcon = async () => {
             try {
                 const importedIcon = await import(`../assets/skill_icons/${props.iconName}`);
@@ -15,13 +16,19 @@ function SkillTile(props) {
             }
         };
 
+        // Set boolean flag so that multi-line skill names will have a fixed width
+        if (props.skillName.indexOf(' ') !== -1)
+        {
+            setIsMultiLine(true);
+        }
+
         loadIcon();
-    }, [props.iconName]);
+    }, [props.iconName, props.skillName]);
 
     return(
-        <div className="inline-flex h-16 w-48 items-center justify-center bg-white-with-pink-tint rounded-lg px-4 py-1">
+        <div className="inline-flex h-16 w-56 items-center justify-center bg-white-with-pink-tint rounded-lg py-1">
             <img src={icon} alt={`${props.skillName} Icon`} />
-            <p className="text-off-black font-bold text-3xl pl-3 leading-7">{props.skillName}</p>
+            <p className={`text-off-black font-bold text-3xl pl-3 leading-7 ${isMultiLine ? "w-32" : ""}`}>{props.skillName}</p>
         </div>
     );
 }
--- src/About/Skills.js
+++ src/About/Skills.js
@@ -2,11 +2,15 @@ import SkillTile from "./SkillTile";
 
 function Skills() {
     const skills = [
-        ["Java", "java.svg"], ["C", "c.svg"], ["C++", "c++.svg"], ["Python", "python.svg"]
+        ["Java", "java.svg"], ["C", "c.svg"], ["C++", "c++.svg"], ["Python", "python.svg"],
+        ["C#", "csharp.svg"], ["Kotlin", "kotlin.svg"], ["JavaScript", "javascript.svg"], ["PHP", "php.svg"],
+        ["HTML", "html.svg"], ["CSS", "css.svg"], ["SQL", "sql.svg"], ["Haskell", "haskell.svg"],
+        ["Android", "android.svg"], ["Android Studio", "android_studio.svg"], ["React", "react.svg"], ["React Native", "react_native.svg"],
+        ["Git", "git.svg"], ["GitHub", "github.svg"], ["Unity", "unity.svg"], ["Agile", "agile.svg"],
     ];
 
     return(
-        <div className="grid justify-items-center mx-6 gap-y-11 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid justify-items-center mx-6 mt-20 gap-y-11 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {skills.map(([skillName, iconName]) => (
                 <SkillTile key={skillName} skillName={skillName} iconName={iconName} />
             ))}
