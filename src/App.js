a007d99d36f5848ef3043d988854366737e8181a
gns12 <garrettspears12@gmail.com>
2024-04-14 15:10:09 -0400

    Started experience section.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from './Nav/Navbar';
 import Home from './Home/Home';
 import About from './About/About';
+import Experience from './Experience/Experience';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <About />
+      <Experience />
     </div>
   );
 }
---
+++ src/Experience/Experience.js
@@ -0,0 +1,13 @@
+import LinePageHeader from "../Utils/LinePageHeader";
+
+function Experience() {
+    return (
+        <section id="experience" className="mt-48">
+          <LinePageHeader headerText="Experience" reverseDirection />
+          <div className="flex flex-col">
+          </div>
+        </section>
+      );
+}
+
+export default Experience;
